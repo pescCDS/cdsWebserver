97e8ad483adaee9d52e5a2ad353123fb2cf035e0
Wes Owen <owenwe@cccnext.net>
2014-08-22 02:36:23 -0700

    directoryServer/src/main/java/org/pesc/cds/directoryserver/

--- directoryServer/src/main/java/org/pesc/cds/webservice/service/WebServiceImpl.java
+++ directoryServer/src/main/java/org/pesc/cds/webservice/service/WebServiceImpl.java
@@ -1,29 +1,42 @@
 package org.pesc.cds.webservice.service;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.hibernate.Session;
 import org.pesc.cds.webservice.service.HibernateUtil;
 import org.pesc.cds.webservice.WebServicesInterface;
-import org.pesc.cds.webservice.domain.Organization;
 import org.pesc.cds.webservice.ServletException;
+import org.pesc.core.coremain.v1_12.UserDefinedExtensionsType;
+import org.pesc.edexchange.v1_0.ContentTypeType;
+import org.pesc.edexchange.v1_0.DeliveryOptionType;
+import org.pesc.sector.academicrecord.v1_7.OrganizationType;
+import org.pesc.sector.academicrecord.v1_7.SourceDestinationType;
+
 
 public class WebServiceImpl implements WebServicesInterface {
 	
 	@Override
-	public List<Organization> getOrganizations() throws ServletException {
+	public List<SourceDestinationType> organizationListRequest(String organizationNameSearchString) throws ServletException {
+		List<SourceDestinationType> retList = new ArrayList<SourceDestinationType>();
 		try {
-			/*Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+			Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 			session.beginTransaction();
 			
-			List<Organization> list = session.createQuery("FROM Organization").list();
+			List<OrganizationType> orgList = session.createQuery("FROM Organizations").list();
+			for(Iterator<OrganizationType> iter = orgList.iterator(); iter.hasNext();) {
+				OrganizationType org = iter.next();
+				SourceDestinationType sdt = new SourceDestinationType();
+				sdt.setOrganization(org);
+				retList.add(sdt);
+			}
 			
 			session.getTransaction().commit();
-			return list;*/
-			return null;
-		}
-		catch (Exception ex) {
-			//HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().rollback();
+			
+			return retList;
+		} catch(Exception ex) {
+			HibernateUtil.getSessionFactory().getCurrentSession().getTransaction().rollback();
 			if ( ServletException.class.isInstance( ex ) ) {
                 throw ( ServletException ) ex;
             }
@@ -32,17 +45,41 @@ public class WebServiceImpl implements WebServicesInterface {
             }
 		}
 	}
-	/*
-	@Override
-	public String getCDSBatch(String psisCode, String username, String password) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public String sendCDSBatch(String psisCode, String username, String password, String cdsBatchXML) {
-		// TODO Auto-generated method stub
-		return null;
+		return "TODO: need content for the sendCDSBatch method";
 	}
-	*/
+
+	@Override
+	public String getCDSBatch(String psisCode, String username, String password) {
+		return "TODO: need content for the getCDSBatch method";
+	}
+
+	@Override
+	public List<DeliveryOptionType> deliveryOptionRequest(
+			List<SourceDestinationType> destinationOrganization,
+			ContentTypeType contentType, List<String> noteMessageText,
+			UserDefinedExtensionsType userDefinedExtensionsText)
+			throws ServletException {
+		
+		List<DeliveryOptionType> retList = new ArrayList<DeliveryOptionType>();
+		return retList;
+	}
+
+	@Override
+	public String getDeliveryLocation() throws ServletException {
+		return "TODO: need content for the getDeliveryLocation method";
+	}
+
+	@Override
+	public String updateLocalOptions() throws ServletException {
+		return "TODO: need content for the updateLocalOptions method";
+	}
+
+	@Override
+	public String updateOrganization(OrganizationType organization) throws ServletException {
+		return "TODO: need content for the updateOrganization method";
+	}
+	
 }
