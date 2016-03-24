ef9402fb0cc90a8960252f5b8bacb181e75dc4e7
James Whetstone <admin@abassitant.com>
2016-03-24 14:12:12 -0700

    Removed unneeded Angular directive.

--- directoryServer/src/main/resources/static/js/app.js
+++ directoryServer/src/main/resources/static/js/app.js
@@ -23,17 +23,6 @@
         };
     });
 
-    app.directive('backButton', ['$window', function($window) {
-        return {
-            restrict: 'A',
-            link: function (scope, elem, attrs) {
-                elem.bind('click', function () {
-                    $window.history.back();
-                });
-            }
-        };
-    }]);
-
     app.directive('toNumber', function() {
         return {
             require: 'ngModel',
