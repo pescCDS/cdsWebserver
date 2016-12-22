77754bdef8625553b61a4e6daf3b0a98629336f0
James Whetstone <jwhetstone@ccctechcenter.org>
2016-12-22 12:40:08 -0800

    Work in progress on using liquibase to make database script portable.

---
+++ directoryServer/src/main/resources/db/db.changelog-0.0.2/1-add-document-formats.sql
@@ -0,0 +1,9 @@
+INSERT INTO document_format (name,description,inuse_count) VALUES
+('PESCXML','PESC’s Core Component Naming convention is based on the Core Component naming convention described in the UNCEFACT Core Component Technical Specification. The UNCEFACT Core Component Technical Specification’s naming convention is based on the standards outlined ISO 11179 Part 5 – Naming and Identification Principles for Data Elements. The Core Component Technical Specification expands upon the ISO 11179 naming convention standards to include Core Component Types and Business Information Entities. ',0),
+('XML','EXtensible Markup Language. ',0),
+('Text','A human-readable format',0),
+('PDF','Adobe Portable Document Format.',0),
+('Image','Binary format that represents a graphical image.',0),
+('Binary','A format intended to be parsed or read by software.',0),
+('EDI','Electronic data interchange. A format that conforms to one of the following standards; UN/EDIFACT, ASC X12, TRADACOMS, ODETTE, VDA.',0),
+('MutuallyDefined','A document format agreed upon by both the sending and receiving party.',0);
